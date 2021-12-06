ab4100873cd73704482a1f967ecfd764c47a92e4
BessThiss <jthinpho@hotmail.com>
2021-12-06 15:58:39 +0100

    Second

--- lib/buffer/buffer.c
+++ lib/buffer/buffer.c
@@ -2,13 +2,18 @@
 
 void buffer_insert(uint8_t *buffer, uint8_t start, uint8_t length, uint64_t value)
 {
-    uint8_t *buf;
-    uint8_t start;
+    uint8_t *buffer;
+    uint8_t start = 87;
     uint8_t length = 90;
-    uint64_t value = (23, 6, 88);
-    
+    uint64_t value[3] = (23, 6, 88);
 }
 
 uint64_t buffer_extract(uint8_t *buffer, uint8_t start, uint8_t length)
 {
+    uint64_t value[3] = {23, 6, 88};
+    int i;
+    for (i = 0; i < value[i]; i++)
+    {
+        printf("%d\n", value[i]);
+    }
 }
--- test/buffer_test.c
+++ test/buffer_test.c
@@ -11,12 +11,28 @@ void tearDown(void) {}
 
 void test_buffer_module(void)
 {
+    uint8_t array[8] = {23, 6, 88};
 
+    TEST_ASSERT_EQUAL_UINT8(2, list_nth_data(1));
+    TEST_ASSERT_EQUAL_UINT8(8, list_nth_data(2));
+    TEST_ASSERT_EQUAL_UINT8(10, list_nth_data(3));
+    TEST_ASSERT_EQUAL_UINT8(10, list_nth_data(4));
+    TEST_ASSERT_EQUAL_UINT8(20, list_nth_data(5));
+
+    TEST_ASSERT_EQUAL_size_t(5, list_available());
+
+    TEST_ASSERT_EQUAL_UINT8(0, list_nth_data(0));
+    TEST_ASSERT_EQUAL_UINT8(0, list_nth_data(10));
+
+    list_clear();
+    TEST_ASSERT_EQUAL_size_t(0, list_available());
 }
 
 int main(void)
 {
     UNITY_BEGIN();
+
+    RUN_TEST(test_buffer_module);
 
     return UNITY_END();
 }
