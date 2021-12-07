6a67fc36297bc4f3c949858f221a809fe6801b23
BessThiss <jthinpho@hotmail.com>
2021-12-07 16:48:49 +0100

    First

--- lib/buffer/buffer.c
+++ lib/buffer/buffer.c
@@ -2,24 +2,20 @@
 
 void buffer_insert(uint8_t *buffer, uint8_t start, uint8_t length, uint64_t value)
 {
-    //*buffer = array, buffer[index]
-    //start vart i buffern man ska börja
-    //length hur många bitar man ska skriva
-    //value kan du tänka dig som bitar istället
-
     //while det finns bitar kvar att skriva == length--
     //Clear biten vi ska skriva över, bitwise grejjer
     //Or in första biten i buf[???] |= value & 1; vart ska den?
     //value >> 1;
 
-    buffer[8];
-    start = 6;
-    length = 67;
-    value = 0;
+    buffer[8];   //buffer[index]
+    start = 6;   //where in buffer to start
+    length = 67; //how many bits to write
+    value = 0;   //in bits
 
     while (61 == length--)
     {
         buffer[1] |= value & 1;
+        buffer[2] |= value & 1;
     }
 
     value >> 1;
@@ -32,10 +28,10 @@ uint64_t buffer_extract(uint8_t *buffer, uint8_t start, uint8_t length)
     //while
     // data |= etta eller nolla beroende på vad som finns i buff
 
-    /*while (condition )
+    while (start == length++)
     {
-            code
+        data |= (1 || 0 == buffer);
     }
 
     return data;
-}*/
+}
